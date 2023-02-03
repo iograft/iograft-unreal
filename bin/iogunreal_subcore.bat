8e5abfefc7baa4acefdedb60455562f59a06af2c
Dan Knowlton <dan@iograft.com>
2023-02-02 19:55:48 -0800

    Update iogunreal_subcore to work with UE5

--- bin/iogunreal_subcore.bat
+++ bin/iogunreal_subcore.bat
@@ -1,8 +1,23 @@
 @echo off
+SETLOCAL EnableDelayedExpansion
 
 :: Build the iograft subcore python script command.
 set IOGRAFT_SUBCORE_COMMAND=%~dp0\iogunreal_subcore.py %*
 
+:: Set which UnrealEditor command to use based on the UE major version.
+set UE4_EDITOR_CMD="UE4Editor-Cmd.exe"
+set UE5_EDITOR_CMD="UnrealEditor-Cmd.exe"
+
+:: Default to Unreal 4.
+set UE_EDITOR_CMD=%UE4_EDITOR_CMD%
+if defined UE_MAJOR_VERSION (
+    if !UE_MAJOR_VERSION!==5 (
+        set UE_EDITOR_CMD=%UE5_EDITOR_CMD%
+    ) else (
+        set UE_EDITOR_CMD=%UE4_EDITOR_CMD%
+    )
+)
+
 :: Launch the Unreal Editor in headless mode. This retrieves the project
 :: from the environment and executes the iograft subcore python script.
-UE4Editor-Cmd.exe "%UE_PROJECT_PATH%" -run=pythonscript -script="%IOGRAFT_SUBCORE_COMMAND%"
+%UE_EDITOR_CMD% "%UE_PROJECT_PATH%" -run=pythonscript -script="%IOGRAFT_SUBCORE_COMMAND%"
