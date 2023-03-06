29f44b47b8883d62a4bad631855e0992728cb273
Dan Knowlton <dan@iograft.com>
2023-03-05 16:45:17 -0800

    Auto-detect Unreal version (4/5); remove requirement for UE_MAJOR_VERSION in environment settings

--- bin/iogunreal_subcore.bat
+++ bin/iogunreal_subcore.bat
@@ -1,21 +1,18 @@
 @echo off
-SETLOCAL EnableDelayedExpansion
 
 :: Build the iograft subcore python script command.
 set IOGRAFT_SUBCORE_COMMAND=%~dp0\iogunreal_subcore.py %*
 
 :: Set which UnrealEditor command to use based on the UE major version.
-set UE4_EDITOR_CMD="UE4Editor-Cmd.exe"
-set UE5_EDITOR_CMD="UnrealEditor-Cmd.exe"
+set UE_EDITOR_CMD="UE4Editor-Cmd.exe"
 
-:: Default to Unreal 4.
-set UE_EDITOR_CMD=%UE4_EDITOR_CMD%
-if defined UE_MAJOR_VERSION (
-    if !UE_MAJOR_VERSION!==5 (
-        set UE_EDITOR_CMD=%UE5_EDITOR_CMD%
-    ) else (
-        set UE_EDITOR_CMD=%UE4_EDITOR_CMD%
-    )
+:: Check for Unreal 5
+set FOUND=
+for %%X in (UnrealEditor-Cmd.exe) do (
+    set "FOUND=%%~$PATH:X"
+)
+if defined FOUND (
+    set UE_EDITOR_CMD="UnrealEditor-Cmd.exe"
 )
 
 :: Launch the Unreal Editor in headless mode. This retrieves the project
